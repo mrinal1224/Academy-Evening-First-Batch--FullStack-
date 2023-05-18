e645428ac6876c77b0a851589c7d6de61f101578
mrinal1224 <iammrinalkc@gmail.com>
2023-05-18 22:25:20 +0530

    push

--- class-14/Kanaban Board/script.js
+++ class-14/Kanaban Board/script.js
@@ -1,9 +1,35 @@
 let addBtn = document.querySelector('.add-btn')
 let modalCont = document.querySelector('.modal-cont')
+let mainCont = document.querySelector('.main-cont')
+let textArea = document.querySelector('.textArea-cont')
+
+let allPriorityColors = document.querySelectorAll('.priority-color')
+
+// let colors = ["lightpink", "lightgreen", "lightblue", "black"];
 
 let addTaskFlag = false
 
+let modalPrioritycolor ='black'
+
 console.log(addTaskFlag)
+
+
+
+
+// Selecting color for your task
+
+allPriorityColors.forEach(function(colorElem){
+    colorElem.addEventListener('click' , function(){
+        allPriorityColors.forEach(function(priorityColorElem){
+            priorityColorElem.classList.remove('active')
+        })
+       colorElem.classList.add('active')
+
+       modalPrioritycolor = colorElem.classList[0] // lightpink
+    })
+
+    
+})
 
 
 addBtn.addEventListener('click' , function(){
@@ -18,3 +44,33 @@ addBtn.addEventListener('click' , function(){
     }
 
 })
+
+
+modalCont.addEventListener('keydown' , function(e){
+    let key = e.key
+
+    if(key==='Shift'){
+        createTicket(modalPrioritycolor) // ticket generation
+        modalCont.style.display = 'none'
+        textArea.value = ''
+
+    }
+})
+
+
+
+
+
+function createTicket(ticketColor){
+    let ticketCont = document.createElement('div')
+    ticketCont.setAttribute('class' , 'ticket-cont')
+
+
+    ticketCont.innerHTML =`<div class=" ${ticketColor} ticket-color"></div>
+    <div class="ticket-id">12345</div>
+    <div class="task-area">Random Task</div>`
+
+    mainCont.appendChild(ticketCont)
+
+    
+}
---
+++ class-14/test.js
@@ -0,0 +1,17 @@
+let arr = [1 , 2, 3 , 4, 5]
+
+
+//map method
+
+
+let doubledArray = arr.map(function(ele){
+    return ele*2
+})
+
+console.log(doubledArray)
+
+
+let FeDoubled = arr.forEach(function(ele){
+    console.log(ele*2)
+})
+
