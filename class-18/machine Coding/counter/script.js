34fa9cd0d385d60da2834029acb7347d6f6d43d4
mrinal1224 <iammrinalkc@gmail.com>
2023-05-25 22:46:49 +0530

    counter

---
+++ class-18/machine Coding/counter/script.js
@@ -0,0 +1,35 @@
+let incrementField = document.getElementById('increment_value')
+let addBtn = document.getElementById('add')
+let substractBtn = document.getElementById('substract')
+let resetBtn = document.getElementById('reset')
+let counterNumber = document.getElementById('number')
+
+let incrementNumber = 1
+
+incrementField.addEventListener('change' , function(e){
+    incrementNumber = parseInt(e.target.value)
+})
+
+addBtn.addEventListener('click' , function(e){
+    let total = parseInt(counterNumber.innerText) + incrementNumber
+
+    counterNumber.innerText = total
+} )
+
+
+
+substractBtn.addEventListener('click' , function(e){
+    let total = parseInt(counterNumber.innerText) - incrementNumber
+
+  if(total <0){
+        total=0
+    }
+
+    counterNumber.innerText = total
+} )
+
+
+
+resetBtn.addEventListener('click' , function(){
+    counterNumber.innerText=0 
+})
