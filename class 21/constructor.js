db9224290292c34edbdb64cbccc1299284735606
mrinal1224 <iammrinalkc@gmail.com>
2023-06-01 23:08:26 +0530

    constructor Fn

---
+++ class 21/constructor.js
@@ -0,0 +1,39 @@
+// let car1 = {
+//     name : 'ferrari',
+//     color : "red",
+//     topSpeed : "1000km/hr"
+// }
+
+
+
+// let car2 = {
+//     name : "BMW",
+//     color : 'White',
+//     topSpeed :'600km/hr'
+// }
+
+
+
+function car(nameParam , colorParam , topSpeedParam){
+    this.name =nameParam
+    this.color = colorParam
+    this.topSpeed = topSpeedParam
+}
+
+
+
+let car1 = new car("Ferrari" , "Red" , '1000km/hr')
+
+console.log(car1)
+
+let car2 = new car("BMW" , 'white' , '600km/hr')
+
+console.log(car)
+
+
+
+// // this - {
+//     name : "Feerrari",
+//     color : "red",
+//     topSpeed : "1000km/hr"
+// }
