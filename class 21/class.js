c15c506e3c5158ae9e0d55aa193ddd0484666e42
mrinal1224 <iammrinalkc@gmail.com>
2023-06-03 22:02:29 +0530

    Inheritance

--- class 21/class.js
+++ class 21/class.js
@@ -4,10 +4,40 @@ class Person{
           this.Age = age
           this.Gender = gender
       }
+
+     hello(){
+          console.log(`${this.Name} says hello`)
+       } 
+     
+     static staticHello(){
+         console.log('This is a static Method')
+     }
+
+
+}
+
+
+class Teacher extends Person{
+    constructor(name , age , subject , classStrength){
+       super(name , age)
+        this.Subject = subject
+        this.ClassStrength = classStrength
+    }
 }
 
 
 let person1 = new Person('Adam' , 24 , "Male")
 
+let teacher1 = new Teacher('Steve' , 36 , 'Mathematics' , 50)
+
+
 console.log(person1)
+console.log(teacher1)
+
+
+
+
+
+
+
 
