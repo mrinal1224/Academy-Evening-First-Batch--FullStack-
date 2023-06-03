e307e9e1a2dfbcc1293cdabfe21c31bdfb9ea169
mrinal1224 <iammrinalkc@gmail.com>
2023-06-03 22:20:07 +0530

    inheritance

--- class 21/class.js
+++ class 21/class.js
@@ -1,8 +1,7 @@
 class Person{
-      constructor(name , age , gender){
+      constructor(name , age){
           this.Name = name
           this.Age = age
-          this.Gender = gender
       }
 
      hello(){
@@ -18,7 +17,7 @@ class Person{
 
 
 class Teacher extends Person{
-    constructor(name , age , subject , classStrength){
+    constructor(name , age, subject , classStrength){
        super(name , age)
         this.Subject = subject
         this.ClassStrength = classStrength
@@ -26,13 +25,24 @@ class Teacher extends Person{
 }
 
 
+class Student extends Person{
+   constructor(name , age , cgpa){
+       super(name , age)
+       this.cgpa = cgpa
+   }
+}
+
+
 let person1 = new Person('Adam' , 24 , "Male")
 
 let teacher1 = new Teacher('Steve' , 36 , 'Mathematics' , 50)
 
+let student1 = new Student('Mark' , 25 , 8.5)
+
 
 console.log(person1)
 console.log(teacher1)
+console.log(student1)
 
 
 
