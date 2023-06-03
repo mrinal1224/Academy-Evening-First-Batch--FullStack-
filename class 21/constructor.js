415fe8d366a0edc0b8cb4aab4e0680d12a1726f6
mrinal1224 <iammrinalkc@gmail.com>
2023-06-03 20:56:00 +0530

    push

--- class 21/constructor.js
+++ class 21/constructor.js
@@ -14,21 +14,43 @@
 
 
 
+
+
+
 function car(nameParam , colorParam , topSpeedParam){
     this.name =nameParam
     this.color = colorParam
     this.topSpeed = topSpeedParam
+
+
+    this.drive = function(){
+        console.log(`I am driving ${this.name} and the color is ${this.color}`)
+    }
+
+
 }
 
 
 
 let car1 = new car("Ferrari" , "Red" , '1000km/hr')
 
+this - {
+    name : 'Ferrari',
+    color : "red" ,
+    topSpeed : '1000km/hr'
+}
+
+
+
 console.log(car1)
+
+car1.drive()
 
 let car2 = new car("BMW" , 'white' , '600km/hr')
 
 console.log(car)
+
+car2.drive()
 
 
 
