7d18130e5751c214cab110968234d5134df43baa
mrinal1224 <iammrinalkc@gmail.com>
2023-06-06 22:12:12 +0530

    push

--- class-22/script.js
+++ class-22/script.js
@@ -5,15 +5,17 @@ function getEmail(){
 }
 
 
-let student = {
-    firstName : 'Adam',
-    lastName : 'Smith',
-    age : 25,
-
-    
+function chooseSubject(sub1 , sub2){
+   return `${sub1} and ${sub2}`
 }
 
-let teacher = {
+function chooseBatches(batch1 , batch2 , batch3){
+  return [batch1 , batch2 , batch3] 
+}
+
+
+
+let teacher1 = {
     firstName :'Steve',
     lastName : 'Rogers',
     age : 35
@@ -21,13 +23,34 @@ let teacher = {
 
 let teacher2 = {
     firstName : 'tony',
-    lastName : 'Stark'
+    lastName : 'Stark',
+    age : 40
 }
 
 
+let arr = ['septBatch' , 'NovBatch' , 'OctBatch']
+
+let arr2 = ['mayBatch' , 'juneBatch' , 'JanBatch']
 
 
-console.log(getEmail.call(teacher) )
+// student.getEmail(teacher)  function Borrowing
+
+//  console.log(getEmail.call(teacher1))  // global function call for diffrent Object
+
+
+console.log(chooseSubject.call(teacher1 , 'Mathematics' , 100 , true))
+
+console.log(chooseSubject.call(teacher2 , 'English' , 'Geography' , false))
+
+console.log(chooseBatches.apply(teacher1 ,arr))
+
+console.log(chooseBatches.apply(teacher2 , arr2))
+
+
+//apply 
+
+
+
 
 
 
@@ -37,3 +60,9 @@ console.log(getEmail.call(teacher) )
 // call method
 
 
+let student = {
+    firstName : 'Adam',
+    lastName : 'Smith',
+    age : 25,
+
+}
