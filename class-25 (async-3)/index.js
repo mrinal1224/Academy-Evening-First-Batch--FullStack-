ca096c87c0e1ba205159693f08d27d848d15a8cd
mrinal1224 <iammrinalkc@gmail.com>
2023-06-13 22:08:22 +0530

    push

--- class-25 (async-3)/index.js
+++ class-25 (async-3)/index.js
@@ -1,3 +1,5 @@
+
+
 function placeOrder(drink){
   return new Promise(function(resolve , reject){
       if(drink ==='coffee'){
@@ -16,17 +18,54 @@ function processOrder(orderPlaced){
     })
 }
 
+function genreateBill(processedOrder){
+    return new Promise(function(resolve){
+        resolve(`${processedOrder} and Bill generated with 200Rs`)
+    })
+}
 
-placeOrder('coffee').then(function(orderstatus){
+
+// placeOrder('coffee').then(function(orderstatus){
+//     console.log(orderstatus)
+//     return orderstatus
+// }).then(function(orderstatus){
+//     let orderIsProccessed = processOrder(orderstatus)
+//     // console.log(orderIsProccessed)
+//     return orderIsProccessed
+//    }).then(function(orderIsProccessed){
+//     console.log(orderIsProccessed)
+//     return orderIsProccessed
+//       }).then(function(orderIsProccessed){
+//     let BillGenerated = genreateBill(orderIsProccessed)
+//     return BillGenerated
+//          }).then(function(BillGenerated){
+//     console.log(BillGenerated)
+// }).catch(function(err){
+//     console.log(err)
+// })
+
+
+
+
+// Async and Await 
+// to use async await you need to create Functions
+
+
+async function serveOrder(){
+    let orderstatus = await placeOrder('coffee')
     console.log(orderstatus)
-    return orderstatus
-}).then(function(orderstatus){
-    let orderIsProccessed = processOrder(orderstatus)
-    // console.log(orderIsProccessed)
-    return orderIsProccessed
-}).then(function(orderIsProccessed){
-    console.log(orderIsProccessed)
-})
+    let processedOrder = await processOrder(orderstatus)
+    console.log(processedOrder)
+    let generatedBill = await genreateBill(processedOrder)
+    console.log(generatedBill)
+
+
+
+}
+
+serveOrder()
+
+
 
 
 
