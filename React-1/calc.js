76fe808e0c7ae3bd3afac7c20b5a7e1b6c6f30e9
mrinal1224 <iammrinalkc@gmail.com>
2023-06-27 21:32:17 +0530

    modularity

---
+++ React-1/calc.js
@@ -0,0 +1,25 @@
+function add(a,b){
+    console.log(a+b)
+}
+
+function sub(a,b){
+    console.log(a-b)
+}
+
+function mul(a,b){
+    console.log(a*b)
+}
+
+function div(a,b){
+    console.log(a/b)
+}
+
+
+module.exports={
+    addition : add,
+    substraction : sub,
+    multiplication : mul,
+    division : div
+}
+
+
---
+++ React-1/moduleTest.js
@@ -0,0 +1,12 @@
+const calculator = require('./calc')
+
+
+calculator.addition(2 , 3)
+
+calculator.multiplication(2 ,3)
+
+calculator.substraction(3 , 1)
+
+calculator.division(10 , 5)
+
+
