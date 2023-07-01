d84dfdb824d742002e238fbebfe9de10d8af388c
mrinal1224 <iammrinalkc@gmail.com>
2023-07-01 22:24:26 +0530

    From submit

--- React-1/demo/src/components/Form.js
+++ React-1/demo/src/components/Form.js
@@ -1,29 +1,35 @@
-import React from 'react'
+import React from "react";
 
-
-import {useState} from 'react'
+import { useState } from "react";
 
 function Form() {
+  const [firstName, setFirstName] = useState("");
 
-    const [firstName , setFirstName] = useState('')
+  let handleChnage = (e) => {
+    // console.log(e);
+     setFirstName(e.target.value);
+  };
 
 
-    let handleChnage =(e)=>{
-        console.log(e)
-        setFirstName(e.target.value)
+  let handleSubmit= (e)=>{
+    e.preventDefault()
+    console.log(firstName)
+  }
 
-    }
+
 
 
   return (
     <div>
-        <h1>This is a Form</h1>
-        <form>
+      <h1>This is a Form</h1>
+      <form onSubmit={handleSubmit}>
         <label>FirstName</label>
-         <input onChange={handleChnage} type='text' value={firstName}></input>
-        </form>
+        <input onChange={handleChnage} type="text" value={firstName}></input>
+
+        <button>Submit Form Button</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Form
+export default Form;
