73425affaa627afc479b483e5d39e95e9cbf657c
mrinal1224 <iammrinalkc@gmail.com>
2023-07-01 22:48:07 +0530

    form handling

--- React-1/demo/src/components/Form.js
+++ React-1/demo/src/components/Form.js
@@ -4,27 +4,39 @@ import { useState } from "react";
 
 function Form() {
   const [firstName, setFirstName] = useState("");
+  const [lastName , setLastName] = useState('')
+  const [pincode , setPincode] = useState('')
 
-  let handleChnage = (e) => {
+  let handleFirstNameChange = (e) => {
     // console.log(e);
      setFirstName(e.target.value);
   };
 
+  let handleLastNameChange = (e)=>{
+      setLastName(e.target.value)
+  }
+
+  let handlePincode = (e)=>{
+    setPincode(e.target.value)
+  }
 
   let handleSubmit= (e)=>{
     e.preventDefault()
-    console.log(firstName)
+    console.log({
+        fName : firstName,
+        lName : lastName,
+        pin : pincode
+    })
   }
-
-
-
 
   return (
     <div>
       <h1>This is a Form</h1>
       <form onSubmit={handleSubmit}>
         <label>FirstName</label>
-        <input onChange={handleChnage} type="text" value={firstName}></input>
+        <input onChange={handleFirstNameChange} type="text" value={firstName}></input>
+        <input onChange={handleLastNameChange} type="text" value={lastName}></input>
+        <input onChange={handlePincode} type="number" value={pincode}></input>
 
         <button>Submit Form Button</button>
       </form>
@@ -33,3 +45,6 @@ function Form() {
 }
 
 export default Form;
+
+
+
