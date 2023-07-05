3b4d5a5c25a94919df2023ecc96eba221f76a583
mrinal1224 <iammrinalkc@gmail.com>
2023-07-05 23:33:33 +0530

    React Router DOM

--- imdb_project/src/App.js
+++ imdb_project/src/App.js
@@ -10,10 +10,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
+     <BrowserRouter>
       <NavBar />
 
-      <BrowserRouter>
-        <Routes>
+      <Routes>
           <Route
             path="/"
             element={
--- imdb_project/src/components/NavBar.js
+++ imdb_project/src/components/NavBar.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React from "react";
 
-import Logo from '../MovieLogo.png'
+import Logo from "../MovieLogo.png";
+
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-<div className='flex border space-x-8 items-center pl-3 py-4'>
+    <div className="flex border space-x-8 items-center pl-3 py-4">
+      <img src={Logo} className="w-[50px]" />
 
-    <img src={Logo} className='w-[50px]'/>
-      
-    <h3 className='text-blue-400'>Movies</h3>
-    <h3 className='text-blue-400'>WatchList</h3>
-  </div>
-  )
+      <Link to="/" className="text-blue-400">
+        Movies
+      </Link>
+      <Link to="/watchlist" className="text-blue-400">
+        WatchList
+      </Link>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
