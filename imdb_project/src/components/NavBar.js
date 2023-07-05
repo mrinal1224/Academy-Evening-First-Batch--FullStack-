15ba931416572d061492330f1b6599d8e2ed3b3b
mrinal1224 <iammrinalkc@gmail.com>
2023-07-05 22:12:57 +0530

    NavBar

--- imdb_project/src/components/NavBar.js
+++ imdb_project/src/components/NavBar.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
+import Logo from '../MovieLogo.png'
+
 function NavBar() {
   return (
 <div className='flex border space-x-8 items-center pl-3 py-4'>
+
+    <img src={Logo} className='w-[50px]'/>
       
     <h3 className='text-blue-400'>Movies</h3>
     <h3 className='text-blue-400'>WatchList</h3>
