042e9268a9b7070abd11c95f0b8d3c6c18153251
mrinal1224 <iammrinalkc@gmail.com>
2023-07-05 22:06:17 +0530

    NavbAr

---
+++ imdb_project/src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <NavBar/>
+  );
+}
+
+export default App;
---
+++ imdb_project/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+<div className='flex border space-x-8 items-center pl-3 py-4'>
+      
+    <h3 className='text-blue-400'>Movies</h3>
+    <h3 className='text-blue-400'>WatchList</h3>
+  </div>
+  )
+}
+
+export default NavBar
